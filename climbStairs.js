c909936937cfa65515c5f4a046a3a5341f77cc8f
csandoval18 <christian_alvites@outlook.com>
2023-01-02 20:09:42 -0600

    practice

---
+++ climbStairs.js
@@ -0,0 +1,17 @@
+const climbStairs = (n) => {
+	let a = 1,
+		b = 1,
+		tmp
+	for (let i = 0; i < n - 1; ++i) {
+		tmp = a
+		a = a + b
+		b = tmp
+	}
+	return a
+}
+
+class MyClass {
+	constructor() {
+		this.data = []
+	}
+}
